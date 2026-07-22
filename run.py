a214839feca6eba3deb5f32c8ab04172de1a8dc5
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-22 15:11:51 +0000

    导航页 favicon 支持多格式：ico/png/svg/jpg/gif，增加下载日志方便排查

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-22 15:02:22"
-ts = 1784732542
+ver = "2026-07-22 15:11:51"
+ts = 1784733111
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
