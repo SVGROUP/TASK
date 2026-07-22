25c1a9b22e458f0c85c3d3ddace5110d98fda9ba
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-22 14:22:39 +0000

    导航页交互重构：统一模式开关(编辑/删除) + 简洁无卡片布局

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-22 14:07:37"
-ts = 1784729257
+ver = "2026-07-22 14:22:39"
+ts = 1784730159
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
