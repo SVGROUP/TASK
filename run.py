5c03f48359d50f488d5c032f44d7c0eb5e7bad7d
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 11:00:24 +0000

    多选框只在编辑模式下显示，保持普通视图干净

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 08:21:18"
-ts = 1784794878
+ver = "2026-07-23 11:00:24"
+ts = 1784804424
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
