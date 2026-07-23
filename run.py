7600d760424e599191fdd71da1c564db051f1a13
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 00:15:31 +0000

    修复 favicon 下载 SSL 证书验证失败问题：跳过 verify

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 00:04:03"
-ts = 1784765043
+ver = "2026-07-23 00:15:31"
+ts = 1784765731
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
