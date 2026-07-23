590d4daf5e62ff7c7c5454cdab4c46f722409056
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 11:11:46 +0000

    calendar JS 版本号 v35 -> v36 避免缓存

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 11:00:24"
-ts = 1784804424
+ver = "2026-07-23 11:11:45"
+ts = 1784805105
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
