5eafe22ad3366498c81b22fde8000eeaed8c22fd
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-23 12:00:26 +0000

    修复日历多选框点击事件冒泡问题 + 升级版本号到v38

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 11:38:32"
-ts = 1784806712
+ver = "2026-07-23 12:00:26"
+ts = 1784808026
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
