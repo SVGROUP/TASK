2bf3b293889b4f37482e0db7af5af32f83df1ae9
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-07-24 02:19:43 +0000

    增加 NasStart 和 NasStop 接口，用于 NAS 状态通知飞书推送

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-23 15:35:43"
-ts = 1784820943
+ver = "2026-07-24 02:19:42"
+ts = 1784859582
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
