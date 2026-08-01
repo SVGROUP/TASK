ad393f3f419e9fdb5333e9b4ff89dde78d758c0f
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-01 00:55:22 +0000

    feat(nav): 图标库支持从本地上传图标文件

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-07-31 19:24:24"
-ts = 1785497064
+ver = "2026-08-01 08:55:21"
+ts = 1785545721
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
