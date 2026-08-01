b7160715580feb9d8bf5c6269e841107235d4d98
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-01 10:57:55 +0000

    fix(nav): 修复弹窗多余空白 + 输入框变短

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-01 08:55:21"
-ts = 1785545721
+ver = "2026-08-01 18:57:55"
+ts = 1785581875
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
