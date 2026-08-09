c68af43e7b1665b90f32f7ff0b5e91fa1892e378
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-09 04:56:44 +0000

    fix(calendar): 压低 flatpickr-calendar z-index 修周/日视图卡死

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-09 12:35:23"
-ts = 1786250123
+ver = "2026-08-09 12:56:44"
+ts = 1786251404
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
