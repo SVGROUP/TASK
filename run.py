854a0e3f01dc13a36d87d74f4655519d3f0213f7
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 13:58:24 +0000

    fix(calendar): end_date picker 容器内样式整改, 不用裸标签

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-13 21:48:34"
-ts = 1786628914
+ver = "2026-08-13 21:58:24"
+ts = 1786629504
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
