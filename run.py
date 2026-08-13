f8c9ff961a0ebdb92d37770a84f6261acb258a64
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 14:18:05 +0000

    fix(calendar): end_date picker 3 按钮改用项目已有 .primary / .ghost 类, 删冗余 CSS

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-13 22:12:13"
-ts = 1786630333
+ver = "2026-08-13 22:18:05"
+ts = 1786630685
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
