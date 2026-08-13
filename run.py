11e7f1ac00c5a9abb6ed7355f072d8f993dd0ad4
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 07:39:49 +0000

    fix(calendar): 修正年循环只生成首年窗口首个日期的 bug

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-09 13:09:01"
-ts = 1786252141
+ver = "2026-08-13 15:39:48"
+ts = 1786606788
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
