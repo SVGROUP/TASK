66cbb45d295869a6c98632869250d26c65c3e1e5
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 12:45:45 +0000

    build: vendor 公共开源库设为编译例外, 原样 cp 不走 terser

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-13 19:43:24"
-ts = 1786621404
+ver = "2026-08-13 20:45:45"
+ts = 1786625145
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
