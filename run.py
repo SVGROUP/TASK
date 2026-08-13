266a29aeb9eeb0c5134af2489827637deb4d2c9a
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 14:05:13 +0000

    fix(calendar): html2canvas 改为本地化 (跟其它 vendor 同模式)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-13 21:58:24"
-ts = 1786629504
+ver = "2026-08-13 22:05:13"
+ts = 1786629913
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
