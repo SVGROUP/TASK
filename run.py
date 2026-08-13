3106ed58e8d007fb059869c8713423227b9dc82d
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 17:16:20 +0000

    fix(calendar): 删 3 个新 picker 的 trigger 按钮 (20th round)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-13 23:45:25"
-ts = 1786635925
+ver = "2026-08-14 01:16:20"
+ts = 1786641380
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
