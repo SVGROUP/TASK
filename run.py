bcbe1eb2d6a956db30ad718c00b54c3145eb4071
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 15:37:02 +0000

    fix(calendar): 修 actForm TDZ 报错 (18th round)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-13 23:31:49"
-ts = 1786635109
+ver = "2026-08-13 23:37:02"
+ts = 1786635422
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
