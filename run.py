9cbbcd12813d61277dc284a50decdab4439a17ad
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-13 13:38:00 +0000

    fix(calendar): 回退 position:fixed, 修 widget 跑画面左上角

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-13 21:31:37"
-ts = 1786627897
+ver = "2026-08-13 21:38:00"
+ts = 1786628280
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
