fabb85f063af28c21873bd4a3de2afb579b4cafe
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 15:39:43 +0000

    46th round: 缩小 td + host 横向占用 (圣威 23:35 报 2 个 UI 问题)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 23:21:35"
-ts = 1786720895
+ver = "2026-08-14 23:39:43"
+ts = 1786721983
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
