62e45c08c5c6b1a3dd66069a2c11e304f0032b54
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 14:48:25 +0000

    44th round: 用纯 CSS 三角替换 glyphicon 字体 (圣威 22:45 关键提示, 根因是没字体)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 22:43:20"
-ts = 1786718600
+ver = "2026-08-14 22:48:25"
+ts = 1786718905
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
