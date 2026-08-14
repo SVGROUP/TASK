fb80a0a67bf2c6729928d256b5a91f165b65989e
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 09:02:35 +0000

    fix(calendar): 3 套 picker fp form 引用错 (34th round)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 16:49:34"
-ts = 1786697374
+ver = "2026-08-14 17:02:35"
+ts = 1786698155
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
