30992c2679f08ef0385c6d367375657554487797
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 09:31:22 +0000

    37th round: 修 5 套 host 段 .list-unstyled/li/li.picker-switch 3 段 selector 错位

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 17:23:54"
-ts = 1786699434
+ver = "2026-08-14 17:31:22"
+ts = 1786699882
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
