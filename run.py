f965d9ba8a98f91fa55e9be2fa079a3f0ecb7905
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 12:30:43 +0000

    41st round: 把开始/结束时间迁移到 bootstrap-datetimepicker timepicker (圣威 20:27 拍板)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 17:52:39"
-ts = 1786701159
+ver = "2026-08-14 20:30:43"
+ts = 1786710643
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
