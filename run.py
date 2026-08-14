488274f20d7cceffe19903347fd00c03244260b4
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 09:45:54 +0000

    39th round: 修 2 个 bug (圣威 17:41 报)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 17:39:00"
-ts = 1786700340
+ver = "2026-08-14 17:45:54"
+ts = 1786700754
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
