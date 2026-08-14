833e7bab5e54c387373276b5347cf64d394b6f53
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 14:43:20 +0000

    43rd round: 修 42nd round 没修干净的 2 个 UI 问题 (圣威 22:39 报)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 20:37:05"
-ts = 1786711025
+ver = "2026-08-14 22:43:20"
+ts = 1786718600
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
