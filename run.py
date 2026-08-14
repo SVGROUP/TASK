1110a6b698fa44ef8f870d05f8a67f4d2b79974f
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 07:09:29 +0000

    fix(calendar): start_date 改为独立 picker dialog, 真复制 2 份独立代码 (17th round 重做)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 14:49:21"
-ts = 1786690161
+ver = "2026-08-14 15:09:29"
+ts = 1786691369
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
