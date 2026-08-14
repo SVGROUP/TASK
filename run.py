aa01900785db41c610c88ba8b99a8024a4742128
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 16:09:24 +0000

    48th round: 修 host 居中 + 三角在 a 标签中居中 (圣威 00:06 报 2 个 UI 问题)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 23:50:30"
-ts = 1786722630
+ver = "2026-08-15 00:09:24"
+ts = 1786723764
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
