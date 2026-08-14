1e22e1046bebfa1534b821c6f6ff24ee6f4dcf1a
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-14 15:50:30 +0000

    47th round: 修 dialog 宽度 + 中间时间居中 (圣威 23:46 报 dialog 宽度问 + 时间不居中)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-14 23:39:43"
-ts = 1786721983
+ver = "2026-08-14 23:50:30"
+ts = 1786722630
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
