4ac7185d2894a497bab254754cefd3c97c3a4cf2
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-08-15 02:43:33 +0000

    51st round: 把 12 段 time picker CSS 扩 4 套 host 命名空间 (圣威 10:39 报 detail 2 套样式都乱了)

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from taskmanage.app import main
 
-ver = "2026-08-15 00:39:16"
-ts = 1786725556
+ver = "2026-08-15 10:43:33"
+ts = 1786761813
 if __name__ == "__main__":
     import os
     os.environ["TASKMANAGE_VERSION"] = ver
